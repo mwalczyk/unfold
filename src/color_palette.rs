fd622fd1648d5b1217e72aa52fb2eab67defb2de
unknown <mwalczyk2@gmail.com>
2020-10-02 14:18:49 -0700

    Adds more references in README. Adds platonic solid models.

--- src/color_palette.rs
+++ src/color_palette.rs
@@ -1,9 +1,13 @@
 use glam::Vec3;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
+/// A struct representing a color palette used for rendering an unfolded net.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ColorPalette {
+    // The background color (RGB)
     pub background: Vec3,
+
+    // A list of colors that will be applied to the faces of the net
     pub polygons: Vec<Vec3>,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-mod goal_mesh;
 mod color_palette;
+mod goal_mesh;
 mod half_edge;
 mod utils;
 
 use std::path::Path;
 
-use crate::goal_mesh::GoalMesh;
 use crate::color_palette::ColorPalette;
+use crate::goal_mesh::GoalMesh;
 use crate::utils::*;
 
 use bevy::prelude::*;
@@ -57,12 +57,15 @@ fn main() {
             clap::Arg::new("WIREFRAME")
                 .about("Sets the draw mode to wireframe (instead of filled)")
                 .short('w')
-                .long("wireframe")
+                .long("wireframe"),
         )
         .get_matches();
 
     // This arg is required, so we can safely unwrap
-    let path_to_obj = matches.value_of("INPUT").expect("This parameter should always be provided").to_owned();
+    let path_to_obj = matches
+        .value_of("INPUT")
+        .expect("This parameter should always be provided")
+        .to_owned();
     info!("Unfolding .obj: {:?}", path_to_obj);
 
     let resolution = matches
@@ -82,18 +85,22 @@ fn main() {
             let json_file = File::open(json_file_path).expect("File not found");
             let deserialized: ColorPalette =
                 serde_json::from_reader(json_file).expect("Error while reading json");
+            if deserialized.polygons.len() == 0 {
+                panic!("Polygon color array is empty: must provide at least one color");
+            }
             deserialized
-        },
+        }
         _ => {
-            ColorPalette::new(&Vec3::new(1.0, 0.98, 0.98),
-                              & vec![
-                                  Vec3::new(0.5568627450980392, 0.792156862745098, 0.9019607843137255),
-                                  Vec3::new(0.12941176470588237, 0.6196078431372549, 0.7372549019607844),
-                                  Vec3::new(0.00784313725490196, 0.18823529411764706, 0.2784313725490196),
-                                  Vec3::new(1.0, 0.7176470588235294, 0.011764705882352941),
-                                  Vec3::new(0.984313725490196, 0.5215686274509804, 0.0),
-                              ]
-
+            // Default color palette
+            ColorPalette::new(
+                &Vec3::new(1.0, 0.98, 0.98),
+                &vec![
+                    Vec3::new(0.5568627450980392, 0.792156862745098, 0.9019607843137255),
+                    Vec3::new(0.12941176470588237, 0.6196078431372549, 0.7372549019607844),
+                    Vec3::new(0.00784313725490196, 0.18823529411764706, 0.2784313725490196),
+                    Vec3::new(1.0, 0.7176470588235294, 0.011764705882352941),
+                    Vec3::new(0.984313725490196, 0.5215686274509804, 0.0),
+                ],
             )
         }
     };
@@ -110,10 +117,12 @@ fn main() {
         .add_resource(WindowDescriptor {
             width: resolution,
             height: resolution,
-            title: String::from("durer"),
+            title: String::from("unfold"),
             ..Default::default()
         })
-        .add_resource(ClearColor(Color::from(input_args.color_palette.background.extend(1.0))))
+        .add_resource(ClearColor(Color::from(
+            input_args.color_palette.background.extend(1.0),
+        )))
         .add_resource(Msaa { samples: 8 })
         .add_resource(input_args)
         .add_default_plugins()
@@ -127,26 +136,30 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     args: Res<InputArgs>,
 ) {
+    // First, construct the goal mesh (and half-edge data structure)
     let mut goal_mesh = GoalMesh::from_obj(&Path::new(&args.path_to_obj[..]), 0.into());
     let mut unfolded_positions = goal_mesh.unfold();
 
     // Make sure that the unfolded net always fits into the specified canvas size
-    // (with padding)
+    // (with PADDING)
+    const PADDING: f32 = 100.0;
     let (net_size_x, net_size_y) = find_extents(&unfolded_positions);
-    let padding = 100.0;
     let net_center = find_centroid(&unfolded_positions);
-    let net_scale = (args.resolution as f32 - padding) / net_size_x.max(net_size_y);
+    let net_scale = (args.resolution as f32 - PADDING) / net_size_x.max(net_size_y);
     info!("Net size: {:?} x {:?}", net_size_x, net_size_y);
     info!("Net center: {:?}", net_center);
     for point in unfolded_positions.iter_mut() {
         *point = (*point - net_center) * net_scale;
     }
+    debug_assert!(unfolded_positions.len() % 3 == 0);
 
-    // Create constant materials
-    let mats = args.color_palette.polygons
+    // Create materials based on the provided color palette
+    let mats = args
+        .color_palette
+        .polygons
         .iter()
         .map(|color| {
-            // Convert SRGB to linear (to compensate for Bevy's internal color conversion)
+            // Convert SRGB to linear (to compensate for Bevy's internal color system)
             let color = Vec3::new(
                 srgb_to_linear(color.x()),
                 srgb_to_linear(color.y()),
